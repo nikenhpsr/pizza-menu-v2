b7bea6341d4decd67cfe3d390d362668f7266bfe
nikenhpsr <niken18003@mail.unpad.ac.id>
2023-10-23 18:37:10 +0700

    fix(order.tsx): declare type of closeHour and openHour

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import Order from "./Order";
 
 export default function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
+  const openHour: number = 12;
+  const closeHour: number = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
